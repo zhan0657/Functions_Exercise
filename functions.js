21fdef084d90188b10bf7300f7177130d78e9b9d
MaddProf1 <davidst@algonquincollege.com>
2020-05-15 16:17:01 -0400

    first commit

---
+++ functions.js
@@ -0,0 +1,87 @@
+// This function returns a random number between (and including both) minimum and maximum
+function getRandomInteger (minimum, maximum) {
+  minimum = Math.ceil(minimum) // minimum is inclusive
+  maximum = Math.floor(maximum + 1) // maximum is exclusive so we add one
+  return Math.floor(Math.random() * (maximum - minimum)) + minimum
+}
+
+// Povided Arrays
+const nouns = ['dragons', 'toasters']
+
+const verbs = ['walked', 'ran']
+
+const adverbs = ['menacingly', 'intentionally']
+
+const adjectives = ['wild', 'slippery']
+
+const proNouns = ['they', 'we', 'you', 'he', 'she', 'I']
+
+const openings = ['Once upon a time', 'I watched']
+
+const closings = ['with extra cheese', 'the end']
+
+// Instructions:
+
+// Arrays:
+// Each provided array (except for proNouns) must have at least 10 elements (words or phrases)
+// Add/replace words to each array based on any theme you like (keep it tasteful!)
+// You do not have to use the example words in any of the provided arrays
+
+// Functions:
+
+// First function:
+// Create a function that outputs the value sent to it using console.log()
+// There should be a newline ("\n") both before and after the value is output
+// Name this function appropriately
+// Note this function does not return any value
+
+// Second function:
+// Create a function that builds a sentence using the 7 provided arrays
+// Using the provided getRandomInteger function you will randomly use content from each array based on the random number returned by getRandomInt.
+// Because you are using an array the minimum value should always be zero
+// The maximum value should be the array length - 1
+//
+// The sentence created (concatinated) inside this function should be structured like this:
+//      Get a random element from the openings array and add it to your new sentence
+//      add a space (and  punctuation if you wish)
+//      next get a random element from the adjectives array and add it to your sentence
+//      add a space (and  punctuation if you wish)
+//      next get a random element from the nouns array and add it to your sentence
+//      add a space (and  punctuation if you wish)
+//      next get a random element from the pro_nouns array and add it to your sentence
+//      add a space (and  punctuation if you wish)
+//      next get a random element from the verbs array and add it to your sentence
+//      add a space (and  punctuation if you wish)
+//      next get a random element from the adverbs array and add it to your sentence
+//      add a space (and  punctuation if you wish)
+//      next get a random element from the adjectives array and add it to your sentence
+//      add closing punctuation.
+
+// Note Feel free to re-arrange your sentence as you wish, just make sure you use the 7 provided arrays
+// Note add more arrays if you wish
+
+// Name this function appropriately
+// This function should return the completed sentence
+
+// Program Flow:
+
+// inside a loop that repeats 10 times
+
+// Your program will call the "sentence creating" function that you are required to build
+// using a new variable you will capture the returned value (the sentence) from this function. Name this variable appropriately
+
+// next you will send this variable to the other function you created used for output
+// this will result in your random sentence being output.
+
+// Other considerations:
+// Everyones code should be different
+// This is a fun exercise so... have fun!
+
+// Submission
+// Submit your complete and TESTED JavaScript file through the LMS. Name your file using this format:
+
+// FirstName_LastName_Function_Assignment.js
+//
+// Example:
+//
+// Tony_Davidson_Function_Assignment.js
