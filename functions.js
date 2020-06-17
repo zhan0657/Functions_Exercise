99423d51b65bc6c269bd967d060175f436804fca
zhan0657@algonquinlive.com <zhan0657@algonquinlive.com>
2020-06-17 16:00:50 -0400

    final commit

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 /*
 Bi Zhang
-June 10, 2020
+June 17, 2020
 */
 
 // This function returns a random number between (and including both) minimum and maximum
@@ -11,76 +11,38 @@ function getRandomInteger (minimum, maximum) {
 }
 
 // Povided Arrays
-const nouns = ['dragons', 'toasters']
+const nouns = ['dragons', 'toasters', 'nuns', 'kings', 'queens', 'princes', 'princesses', 'servants', 'monks', 'knights']
 
-const verbs = ['walked', 'ran']
+const verbs = ['bit', 'cooked', 'laughed at', 'danced with', 'broke', 'bought', 'cursed', 'hit', 'ate', 'killed']
 
-const adverbs = ['menacingly', 'intentionally']
+const adverbs = ['menacingly', 'intentionally', 'peacefully', 'gratefully', 'occasionally', 'mercifully', 'constantly', 'eventually', 'directly', 'badly']
 
-const adjectives = ['wild', 'slippery']
+const adjectives = ['wild', 'fantastic', 'delicious', 'cold', 'evil', 'greedy', 'romantic', 'dishonest', 'careless', 'brutal']
 
 const proNouns = ['they', 'we', 'you', 'he', 'she', 'I', 'it']
 
-const openings = ['Once upon a time', 'I watched']
+const openings = ['Once upon a time', 'I watched', 'Yesterday', 'Last summer', 'This morning', 'Last century', 'last Christmas', 'Do you believe that', 'I bet you never saw', 'I heard that']
 
-const closings = ['with extra cheese', 'the end']
+const closings = ['with extra cheese', 'with COVID-19', 'in the sky', 'on TV', 'in your bedroom', 'with fried chicken', 'in the classroom', 'on the roof', 'with a spoon', 'on facebook']
 
-// Instructions:
 
-// Arrays:
-// Each provided array (except for proNouns) must have at least 10 elements (words or phrases)
-// Add/replace words to each array based on any theme you like (keep it tasteful!)
-// You do not have to use the example words in any of the provided arrays
+function createRandomSentence (){
+  const randomSentence = openings[ getRandomInteger(0, openings.length - 1)] + ' ' + 
+  proNouns[ getRandomInteger(0, proNouns.length - 1)] + ' ' +
+  verbs[ getRandomInteger(0, verbs.length - 1)] + ' ' +
+  adjectives[ getRandomInteger(0, adjectives.length - 1)] + ' ' +
+  nouns[ getRandomInteger(0, nouns.length - 1)] + ' ' +
+  adverbs[ getRandomInteger(0, adverbs.length - 1)] + ' ' +
+  closings[ getRandomInteger(0, closings.length - 1)] + '.' 
 
-// Functions:
+  return randomSentence
+}
 
-// First function:
-// Create a function that outputs the text/value sent to it using console.log()
-// There should be a newline ("\n") both before and after the value is output
-// Name this function appropriately
-// Note this function does not return any value
+function outputSentence(sentence){
+  console.log(sentence)
+}
 
-// Second function:
-// Create a function that builds a sentence using the 7 provided arrays
-// Using the provided getRandomInteger function you will randomly use content from each array based on the random number returned by getRandomInt.
-// Because you are using an array the minimum value should always be zero
-// The maximum value should be the array length - 1
-
-// The sentence created (concatinated) inside this function should be structured like this:
-//      Get a random element from the openings array and add it to your new sentence
-//      add a space (and  punctuation if you wish)
-//      next get a random element from the adjectives array and add it to your sentence
-//      add a space (and  punctuation if you wish)
-//      next get a random element from the nouns array and add it to your sentence
-//      add a space (and  punctuation if you wish)
-//      next get a random element from the pro_nouns array and add it to your sentence
-//      add a space (and  punctuation if you wish)
-//      next get a random element from the verbs array and add it to your sentence
-//      add a space (and  punctuation if you wish)
-//      next get a random element from the adverbs array and add it to your sentence
-//      add a space (and  punctuation if you wish)
-//      next get a random element from the adjectives array and add it to your sentence
-//      add closing punctuation.
-
-// Note Feel free to re-arrange your sentence as you wish, just make sure you use the 7 provided arrays
-// Note add more arrays if you wish
-
-// Name this function appropriately
-// This function should return the completed sentence
-
-// Program Flow:
-
-// inside a loop that repeats 10 times
-
-// Your program will call the "sentence creating" function that you are required to build
-// using a new variable you will capture the returned value (the sentence) from this function. Name this variable appropriately
-
-// next you will send this variable to the other function you created used for output
-// this will result in your random sentence being output.
-
-// Other considerations:
-// Everyones code should be different
-// This is a fun exercise so... have fun!
-
-// Submission
-// Submit your GitHub repo URL through the LMS.
+const numbeOfSentence = 10
+for(let i = 0; i < 10; i++){
+  outputSentence(createRandomSentence())
+}
